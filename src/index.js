2407dd8543814af322761b0f7c51edb634afdec0
A.Seregin <a.siarohin@gmail.com>
2018-09-29 09:29:09 +0300

    advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-  //
+
+ var prime, notation, zerosCount, zerosCountArr, result;
+ notation = base;
+ var prime = {};
+ 	 for (var i = 2; i <= notation; i++) {
+  		while (notation % i === 0) {
+  			if (prime[i]) {
+  				prime[i] = prime[i] + 1;
+  			} else {
+  				prime[i] = 1;
+  			}
+  			notation = notation/i;
+  		}
+  	}
+	function zerosByPrime(number, prime, count) {
+		zerosCount = 0;
+		while(number>0){
+			number = Math.floor(number/prime);
+			zerosCount+=number;
+		}
+	 	return Math.floor(zerosCount/count);	
+	}
+	zerosCountArr = [];
+		for(var key in prime){
+			zerosCountArr.push(zerosByPrime(number, +key, prime[key]));
+		}
+	result = Math.min.apply(null, zerosCountArr);
+	
+return result;
+
 }
