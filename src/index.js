7f8666d5144e229a64a7da603f46ac51b7e0a24a
A.Seregin <a.siarohin@gmail.com>
2018-10-04 21:56:12 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
 module.exports = function getZerosCount(number, base) {
 
- var prime, notation, zerosCount, zerosCountArr, result;
- notation = base;
- var prime = {};
- 	 for (var i = 2; i <= notation; i++) {
-  		while (notation % i === 0) {
-  			if (prime[i]) {
-  				prime[i] = prime[i] + 1;
-  			} else {
-  				prime[i] = 1;
-  			}
-  			notation = notation/i;
+	let prime = {};
+
+ 	for (i = 2; i <= base; i++) {
+
+  		while (base % i === 0) {
+  			prime[i] ? prime[i] += 1 : prime[i] = 1;
+  			base /= i;
   		}
   	}
+
 	function zerosByPrime(number, prime, count) {
-		zerosCount = 0;
-		while(number>0){
-			number = Math.floor(number/prime);
-			zerosCount+=number;
+
+	let zerosCount = 0;
+		while(number > 0){
+			number = Math.floor(number / prime);
+			zerosCount += number;
 		}
-	 	return Math.floor(zerosCount/count);	
+
+	 	return Math.floor(zerosCount / count);	
 	}
+
 	zerosCountArr = [];
-		for(var key in prime){
+		for(key in prime){
 			zerosCountArr.push(zerosByPrime(number, +key, prime[key]));
 		}
-	result = Math.min.apply(null, zerosCountArr);
-	
-return result;
+
+	return Math.min.apply(null, zerosCountArr);
 
 }
